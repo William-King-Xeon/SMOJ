af2ad4f2d5b6f5dab27e2581ae22f64340f67648
William-King-Xeon <xeon-king@qq.com>
2018-11-06 11:17:37 +0800

    2018.11.6

--- 2704.cpp
+++ 2704.cpp
@@ -1,13 +1,52 @@
 #include <cstdio>
+#include <queue>
 
-int n,m,S[2000005];
+using namespace std;
+
+struct EDGE
+{
+    int t,n,l;
+} edge[500005];
+
+int n,m,S[2000005],
+    cur,last[505],
+    f[505],InQueue[505];
+queue<int> q;
+
+void AddEdge(int u,int v,int w)
+{
+    cur++;
+    edge[cur].t=v;
+    edge[cur].n=last[u];
+    edge[cur].l=w;
+    last[u]=cur;
+    return;
+}
 
 void BFS()
+{
+    q.push(1); f[1]=0; InQueue[1]=true;
+
+    while (!q.empty())
+    {
+        int u=q.front(); q.pop(); InQueue[u]=false;
+        for (int tmp=last[u];tmp;tmp=edge[tmp].n)
+        {
+            int v=edge[tmp].t,w=edge[tmp].l;
+            if (S[f[u]]==w && f[u]+1>f[v])
+            {
+                f[v]=f[u]+1;
+                if (!InQueue[v])
+                    q.push(v),InQueue[v]=true;
+            }
+        }
+    }
+}
 
 int main()
 {
-    //freopen("2704.in","r",stdin);
-    //freopen("2704.out","w",stdout);
+    freopen("2704.in","r",stdin);
+    freopen("2704.out","w",stdout);
 
     S[0]=0,S[1]=1;
 
@@ -25,6 +64,9 @@ int main()
 
     BFS();
 
+    int ans=0;
+    for (int i=1;i<=n;i++)
+        if (f[i]>ans) ans=f[i];
     printf("%d\n",ans);
 
     return 0;
